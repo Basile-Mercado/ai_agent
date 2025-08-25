cfb7326461622259322d9c6e734b6c85bc1e85c5
Basile-Mercado <basile.mercado@gmail.com>
2025-08-25 15:22:46 +0200

    add verbose handling

--- main.py
+++ main.py
@@ -18,15 +18,20 @@ def main():
     
     api_key = os.getenv("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    
-    generate_content(client, messages)    
-    
 
-def generate_content(client, messages):
+    if any([arg == '-v' or arg== '--verbose' for arg in args]):
+        generate_content(client, messages, verbose=True)
+    generate_content(client, messages)
+
+def generate_content(client, messages, verbose=False):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages
     )
+    if verbose:
+        print(f"User prompt: {messages[0].parts[0].text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: {response.text}")
 
 if __name__ == "__main__":
